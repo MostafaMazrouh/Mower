4b47368ee2cc7d6f79d6dc04e6603d486c424e8d
Mostafa Mazrouh <Mostafa.Mazrouh@gmail.com>
2024-10-30 09:06:01 +0100

    Initial Commit

---
+++ Mower/MowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MowerApp.swift
+//  Mower
+//
+//  Created by Mostafa Mazrouh on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct MowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MowerTests/MowerTests.swift
@@ -0,0 +1,16 @@
+//
+//  MowerTests.swift
+//  MowerTests
+//
+//  Created by Mostafa Mazrouh on 2024-10-30.
+//
+
+import Testing
+
+struct MowerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
